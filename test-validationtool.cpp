db95bac32e03262ecc2261cb73f7903349cfef78
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-03-24 20:58:00 +0530

    To avoid duplications in test cases

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -8,7 +8,7 @@ struct SensorProp_s SensorProperties[NUMSENSOR] = {
 												   {"Curent" , TOLERANCE_CURRENT}};
 
 /* Test for noisy Current sensor*/
-TEST_CASE("reports error no sensor data is read") 
+TEST_CASE("reports error no sensor data is read - Current sensor") 
 {
   double *socReadings = NULL;
   int numOfSocReadings = NumOfReadingsFromSensor(socReadings) ;
@@ -16,21 +16,21 @@ TEST_CASE("reports error no sensor data is read")
   REQUIRE(validateSensorBasedOnReadings(socReadings, numOfSocReadings, SensorProperties[CURRENT]) == INVALID_DATA);
 }
 
-TEST_CASE("reports error when current jumps abruptly") 
+TEST_CASE("reports error when current jumps abruptly - Current sensor") 
 {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSensorBasedOnReadings(currentReadings, numOfCurReadings, SensorProperties[CURRENT]) == NOISY);
 }
 
-TEST_CASE("noiseless current sensor") 
+TEST_CASE("noise less readings - Current sensor") 
 {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.04, 0.14, 0.16,0.8};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSensorBasedOnReadings(currentReadings, numOfCurReadings, SensorProperties[CURRENT]) == NOISE_FREE);
 }
 
-TEST_CASE("reports error when current jumps abruptly") 
+TEST_CASE("reports error when current jumps abruptly - Current sensor")  
 {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.04, 0.14, 0.16,0.05};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
@@ -39,15 +39,14 @@ TEST_CASE("reports error when current jumps abruptly")
 /*End of test for Current sensor*/
 
 /* Test for noisy SOC sensor*/
-TEST_CASE("reports error when soc jumps abruptly") 
+TEST_CASE("reports error when soc jumps abruptly - SoC sensor") 
 {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = NumOfReadingsFromSensor(socReadings);
   REQUIRE(validateSensorBasedOnReadings(socReadings, numOfSocReadings,SensorProperties[SOC]) == NOISY);
 }
 
-/* Tests for SOC sensor*/
-TEST_CASE("reports error when soc jumps abruptly,both increase and decrease")
+TEST_CASE("noise less readings - Soc sensor")
 {
   double socReadings[] = {0.08, 0.05, 0.1, 0.06, 0.09};
   int numOfSocReadings = NumOfReadingsFromSensor(socReadings);
@@ -55,25 +54,17 @@ TEST_CASE("reports error when soc jumps abruptly,both increase and decrease")
   REQUIRE(validateSensorBasedOnReadings(socReadings, numOfSocReadings,SensorProperties[SOC]) == NOISE_FREE);
 }
 
-/* Test for noisy SOC sensor*/
-TEST_CASE("reports error when soc drops abruptly") 
+TEST_CASE("reports error when soc drops abruptly - SoC sensor") 
 {
   double socReadings[] = {1, 1.04, 0.98, 1.02};
   int numOfSocReadings = NumOfReadingsFromSensor(socReadings);
   REQUIRE(validateSensorBasedOnReadings(socReadings, numOfSocReadings,SensorProperties[SOC]) == NOISY);
 }
 
-/* Test for no readings from SOC sensor*/
-TEST_CASE("reports error no sensor data is read") {
+TEST_CASE("reports error no sensor data is read - SoC sensor") {
   double *socReadings = NULL;
   int numOfSocReadings = NumOfReadingsFromSensor(socReadings) ;
   printf("\n%d", numOfSocReadings);
   REQUIRE(validateSensorBasedOnReadings(socReadings, numOfSocReadings, SensorProperties[SOC]) == INVALID_DATA);
-}
-/* Test for noisy Current sensor*/
-TEST_CASE("reports error when current jumps abruptly") {
-  double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
-  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(validateSensorBasedOnReadings(currentReadings, numOfCurReadings, SensorProperties[CURRENT]) == 0);
 }
 /*End of test for SoC sensor*/
